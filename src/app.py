fa89ee0aadae631e6e2bdbe8a093652658121706
Wesley Dias <wesd_444@hotmail.com>
2022-04-11 19:32:18 -0300

    alteração descrição de métrica

--- src/app.py
+++ src/app.py
@@ -16,7 +16,7 @@ prom = PrometheusConnect(url=os.getenv('PROMETHEUS_URL'), disable_ssl=True)
 btalert_ai_prediction = Gauge('btalert_ai_prediction', 'estimated time in hours for the system to be down')
 btalert_ai_prediction.set(1)
 
-btalert_ai_total_metrics = Gauge('btalert_ai_total_metrics', 'total of metrics available on promethus')
+btalert_ai_total_metrics = Gauge('btalert_ai_total_metrics', 'total of metrics available on prometheus')
 btalert_ai_total_metrics.set(len(prom.all_metrics()))
 
 app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
