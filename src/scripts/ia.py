7195c3bc896d137ca52d0b028f32e6395f496f12
Denis Lima <denislima99@gmail.com>
2022-05-09 12:40:20 -0300

    Transformando IA em classe

--- src/scripts/ia.py
+++ src/scripts/ia.py
@@ -1,42 +1,110 @@
 import os
-from prometheus_client import Gauge
-from prometheus_api_client import PrometheusConnect, MetricSnapshotDataFrame
-from prometheus_api_client.utils import parse_datetime
-from dotenv import load_dotenv
-import pandas as pd
-from datetime import datetime
-from datetime import timedelta
+import warnings
+from datetime import datetime, timedelta
+from typing import Tuple
+
 import matplotlib.pyplot as plt
+import pandas as pd
+from dotenv import load_dotenv
+from prometheus_api_client import MetricSnapshotDataFrame, PrometheusConnect
+from prometheus_api_client.utils import parse_datetime
+from skforecast.ForecasterAutoreg import ForecasterAutoreg
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
 plt.style.use('fivethirtyeight')
 plt.rcParams['lines.linewidth'] = 1.5
 
+
+warnings.filterwarnings('ignore')
+
+
 load_dotenv()
-from skforecast.ForecasterAutoreg import ForecasterAutoreg
-ForecasterAutoreg().fit
-prom = PrometheusConnect(url=os.getenv('PROMETHEUS_URL'), disable_ssl=True)
-
-start_time = parse_datetime("12h")
-end_time = parse_datetime("now")
-
-requests_seconds = MetricSnapshotDataFrame(prom.get_metric_range_data('nginx_vts_server_request_seconds', start_time=start_time, end_time=end_time, label_config={'host': '172.16.2.108'}, chunk_size=timedelta(seconds=15)))
 
 
-req_dataframe = requests_seconds[['timestamp', 'value']].copy()
-req_dataframe['timestamp'] = [datetime.fromtimestamp(timestamp) for timestamp in req_dataframe['timestamp']]
-req_dataframe['value'] = [float(value) for value in req_dataframe['value']]
+class BtalertIA:
+    def __init__(self, last_minutes_importance: int, regressor=None) -> None:
+        """
+        Args:
+            last_minutes_importance (int): The last minutes that matter to foreacasting (context)
+        """
+        self.prom = PrometheusConnect(
+            url=os.getenv('PROMETHEUS_URL'), disable_ssl=True)
+        self.regressor = regressor
+        if regressor is None:
+            self.regressor = RandomForestRegressor(
+                max_depth=40,
+                n_estimators=3,
+                random_state=123,
+            )
+        self.forecaster = ForecasterAutoreg(
+            regressor=self.regressor,
+            lags=self.minutes_to_step(last_minutes_importance)
+        )
+        self.original_dataframe = pd.DataFrame()
+        self.data_train = pd.DataFrame()
+        self.data_test = pd.DataFrame()
+        self.predictions = pd.Series()
+        self.value_column = 'value'
+        self.timestamp_column = 'timestamp'
 
-req_dataframe = req_dataframe.set_index('timestamp')
+    def load_metric_as_dataframe(self, start: str, end: str, metric_name: str) -> pd.DataFrame:
+        start_time = parse_datetime(start)
+        end_time = parse_datetime(end)
+        original_dataframe = MetricSnapshotDataFrame(
+            self.prom.get_metric_range_data(
+                metric_name, start_time=start_time, end_time=end_time, chunk_size=timedelta(seconds=15))
+        )
 
-#print(req_dataframe.head())
+        new_dataframe = original_dataframe[[
+            self.timestamp_column, self.value_column]].copy()
+        new_dataframe[self.timestamp_column] = [datetime.fromtimestamp(
+            timestamp) for timestamp in new_dataframe[self.timestamp_column]]
+        new_dataframe[self.value_column] = [
+            float(value) for value in new_dataframe[self.value_column]]
 
-steps = 36
-data_train = req_dataframe[:-steps]
-data_test  = req_dataframe[-steps:]
+        new_dataframe[self.timestamp_column] = new_dataframe[self.timestamp_column].astype(
+            'datetime64[s]')
+        new_dataframe = new_dataframe.set_index(
+            new_dataframe[self.timestamp_column])
 
-print(f"Train dates : {data_train.index.min()} --- {data_train.index.max()}  (n={len(data_train)})")
-print(f"Test dates  : {data_test.index.min()} --- {data_test.index.max()}  (n={len(data_test)})")
+        new_dataframe = new_dataframe.asfreq(freq='15S', method='bfill')
+        self.original_dataframe = new_dataframe
+        return new_dataframe
 
-fig, ax=plt.subplots(figsize=(9, 4))
-data_train['value'].plot(ax=ax, label='train')
-data_test['value'].plot(ax=ax, label='test')
-ax.legend()
+    def split_test_train_dataframe(self, minutes_split: int) -> Tuple[pd.DataFrame, pd.DataFrame]:
+        steps = self.minutes_to_step(minutes_split)
+
+        self.data_train = self.original_dataframe[:-steps]
+        self.data_test = self.original_dataframe[-steps:]
+
+        return self.data_train, self.data_test
+
+    def minutes_to_step(self, min: int) -> int:
+        return int((min * 60) / 15)
+
+    def train_model(self) -> None:
+        self.forecaster.fit(y=self.data_train[self.value_label])
+
+    def predict(self, minutes_prediction: int) -> pd.Series:
+        return self.forecaster.predict(steps=self.minutes_to_step(minutes_prediction))
+
+    def plot_graphic(self):
+        fig, ax = plt.subplots(figsize=(18, 12))
+        self.data_train[self.value_column].plot(ax=ax, label='train')
+        self.data_test[self.value_column].plot(ax=ax, label='test')
+        self.predictions.plot(ax=ax, label='predictions')
+        ax.legend()
+
+    def get_mean_squared_error(self) -> float:
+        error_mse: float = mean_squared_error(
+            y_true=self.data_test[self.value_column],
+            y_pred=self.predictions
+        )
+        return error_mse
+
+    def execute(self, start: str, end: str, metric_name: str, minutes_split: int):
+        self.load_metric_as_dataframe(start, end, metric_name)
+        self.split_test_train_dataframe(minutes_split)
+        self.train_model()
+        self.predict()
